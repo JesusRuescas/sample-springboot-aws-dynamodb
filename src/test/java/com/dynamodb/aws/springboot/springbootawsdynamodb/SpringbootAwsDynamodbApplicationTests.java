8c11f687ad680d17964c61245a44fd5d012a5698
Jesus Ruescas Junior <jesusruescasjunior@gmail.com>
2023-04-17 15:19:07 -0300

    Add test

---
+++ src/test/java/com/dynamodb/aws/springboot/springbootawsdynamodb/SpringbootAwsDynamodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dynamodb.aws.springboot.springbootawsdynamodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootAwsDynamodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
